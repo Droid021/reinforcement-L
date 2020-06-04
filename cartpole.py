4299849f4ab69b1aa1dd47074531dbd3de246b68
Droid021 <vincivenv@gmail.com>
2020-06-04 16:00:14 +0300

    Running and plot avgs

--- cartpole.py
+++ cartpole.py
@@ -1,29 +1,31 @@
 import gym
 import numpy as np
 import matplotlib.pyplot as plt
-from gym import wrappers
 
 env = gym.make('CartPole-v0')
-MAX_STATES = 10 ** 4
+
+MAXSTATES = 10**4
 GAMMA = 0.9
 ALPHA = 0.01
 
 
 def max_dict(d):
     max_v = float('-inf')
-    for key, value in d.items:
-        if value == max_v:
-            max_v = value
+    for key, val in d.items():
+        if val > max_v:
+            max_v = val
             max_key = key
-        return max_key, max_v
+    return max_key, max_v
 
 
 def create_bins():
+
     bins = np.zeros((4, 10))
     bins[0] = np.linspace(-4.8, 4.8, 10)
-    bins[1] = np.linspace(5, 5, 10)
-    bins[2] = np.linspace(-0.418, 0.418, 10)
-    bins[3] = np.linspace(5, 5, 10)
+    bins[1] = np.linspace(-5, 5, 10)
+    bins[2] = np.linspace(-.418, .418, 10)
+    bins[3] = np.linspace(-5, 5, 10)
+
     return bins
 
 
@@ -35,65 +37,90 @@ def assign_bins(observation, bins):
 
 
 def get_state_as_string(state):
-    str_state = ' '.join(str(int(e)) for e in state)
-    return str_state
+    string_state = ''.join(str(int(e)) for e in state)
+    return string_state
 
 
-def get_all_states_as_strings():
+def get_all_states_as_string():
     states = []
-    for i in range(MAX_STATES):
+    for i in range(MAXSTATES):
         states.append(str(i).zfill(4))
     return states
 
 
 def initialize_Q():
     Q = {}
-    all_states = get_all_states_as_strings()
+
+    all_states = get_all_states_as_string()
     for state in all_states:
         Q[state] = {}
         for action in range(env.action_space.n):
             Q[state][action] = 0
-
     return Q
 
 
-def play_one_game(bins, Q, epsilon=0.5):
+def play_one_game(bins, Q, eps=0.5):
     observation = env.reset()
     done = False
-    count = 0
-    env = wrappers.Monitor(env, 'movie_files/Q_learning', force=True)
+    cnt = 0  # number of moves in an episode
     state = get_state_as_string(assign_bins(observation, bins))
     total_reward = 0
 
     while not done:
-        count += 1
-        if np.random.uniform() < epsilon:
-            action = env.action_space.sample()
+        cnt += 1
+        # np.random.randn() seems to yield a random action 50% of the time ?
+        if np.random.uniform() < eps:
+            act = env.action_space.sample()  # epsilon greedy
         else:
-            action = max_dict(Q[state])[0]
-        observation, reward, done, _ = env.step(action)
+            act = max_dict(Q[state])[0]
+
+        observation, reward, done, _ = env.step(act)
+
         total_reward += reward
-        if done and count < 200:
+
+        if done and cnt < 200:
             reward = -300
+
         state_new = get_state_as_string(assign_bins(observation, bins))
 
-        a1, max_q_sla1 = max_dict(Q[state_new])
-        Q[state][action] = ALPHA * \
-            (reward * GAMMA * max_q_sla1 - Q[state][action])
+        a1, max_q_s1a1 = max_dict(Q[state_new])
+        Q[state][act] += ALPHA*(reward + GAMMA*max_q_s1a1 - Q[state][act])
+        state, act = state_new, a1
 
-        state, action = state_new, a1
-    return total_reward, count
+    return total_reward, cnt
 
 
-def play_many_games(bins, N=1000):
+def play_many_games(bins, N=10000):
     Q = initialize_Q()
+
     length = []
     reward = []
     for n in range(N):
-        epsilon = 1.0/np.sqrt(n+1)
-        episode_reward, episode_length = play_one_game(bins, Q, epsilon)
+        # eps=0.5/(1+n*10e-3)
+        eps = 1.0 / np.sqrt(n+1)
+
+        episode_reward, episode_length = play_one_game(bins, Q, eps)
+
         if n % 100 == 0:
             print(n, '%.4f' % eps, episode_reward)
         length.append(episode_length)
         reward.append(episode_reward)
+
     return length, reward
+
+
+def plot_running_avg(totalrewards):
+    N = len(totalrewards)
+    running_avg = np.empty(N)
+    for t in range(N):
+        running_avg[t] = np.mean(totalrewards[max(0, t-100):(t+1)])
+    plt.plot(running_avg)
+    plt.title("Running Average")
+    plt.show()
+
+
+if __name__ == '__main__':
+    bins = create_bins()
+    episode_lengths, episode_rewards = play_many_games(bins)
+
+    plot_running_avg(episode_rewards)
