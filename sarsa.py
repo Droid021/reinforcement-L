13472353702f21f02abea7adac45b6e613104bcd
Droid021 <vincivenv@gmail.com>
2020-06-06 13:16:19 +0300

    Get state

--- sarsa.py
+++ sarsa.py
@@ -7,3 +7,20 @@ def max_action(Q, state):
     values = np.array([Q[state, a]for a in range(2)])
     action = np.argmax(values)
     return action
+
+
+# discretize the spaces
+poleThetaSpace = np.linspace(-0.20943951, 0.20943951, 10)
+poleThetaVelSpace = np.linspace(-4, 4, 10)
+cartPosSpace = np.linspace(-2.4, 2.4, 10)
+cartVelSpace = np.linspace(-4, 4, 10)
+
+
+def getState(observation):
+    cartX, cartXdot, cartTheta, cartThetadot = observation
+    cartX = int(np.digitize(cartX, cartPosSpace))
+    cartXdot = int(np.digitize(cartXdot, cartVelSpace))
+    cartTheta = int(np.digitize(cartTheta, poleThetaSpace))
+    cartThetadot = int(np.digitize(cartThetadot, poleThetaVelSpace))
+
+    return (cartX, cartXdot, cartTheta, cartThetadot)
